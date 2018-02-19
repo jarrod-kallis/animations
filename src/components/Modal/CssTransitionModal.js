bdcf14321f971aa8c0bae96520b98d1a21f80c26
Jarrod Kallis <jarrod.kallis@altron.com>
2018-02-19 10:25:24 +0200

    CSSTransition component: Assign JS object to classNames

--- src/components/Modal/CssTransitionModal.js
+++ src/components/Modal/CssTransitionModal.js
@@ -10,8 +10,20 @@ const cssModal = props => {
       unmountOnExit
       in={props.show}
       timeout={{ enter: 500, exit: 1000 }}
-      classNames="Modal-Fade-Slide" // Define the base of the css class name.
-      // CSSTransition will append the leaf part of the name. Eg. -enter, -enter-active, -exit, -exit-active
+      /** Define the base of the css class name.
+       CSSTransition will append the leaf part of the name. Eg. -enter, -enter-active, -exit, -exit-active **/
+      // classNames="Modal-Fade-Slide"
+
+      /** Can also assign a JS object **/
+      classNames={{
+        enter: '',
+        enterActive: 'ModalOpen',
+        exit: '',
+        exitActive: 'ModalClosed'
+        // Called when the object is first mounted on the DOM
+        // appear: '',
+        // appearActive: ''
+      }}
     >
       <div className="Modal">
         <h1>A Modal</h1>
