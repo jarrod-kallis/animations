00a9ba6f46f363061dc1931c25625a0309348eb2
Jarrod Kallis <jarrod.kallis@altron.com>
2018-02-19 09:20:51 +0200

    Transition lifecycle methods to handle animations

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -10,6 +10,12 @@ const modal = props => {
       unmountOnExit
       in={props.show}
       timeout={{ enter: 500, exit: 1000 }}
+      onEnter={() => console.log('Transition onEnter')}
+      onEntering={() => console.log('Transition onEntering')}
+      onEntered={() => console.log('Transition onEntered')}
+      onExit={() => console.log('Transition onExit')}
+      onExiting={() => console.log('Transition onExiting')}
+      onExited={() => console.log('Transition onExited')}
     >
       {/* Method executed using the children property of Transition which gets state of the transition.
           eg. entering, entered, exiting, exited */
